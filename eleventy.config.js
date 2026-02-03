1666d7d890f409445aa91e7fff95d63211cb6102
Davidthm96 <45799460+Davidthm96@users.noreply.github.com>
2026-02-04 00:58:29 +0100

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,10 @@
 module.exports = function(eleventyConfig) {
-    // Copia direttamente la cartella delle immagini nel sito finale
-    eleventyConfig.addPassthroughCopy("img");
-
-    // Configura la cartella di input e output
-    return {
-        dir: {
-            input: ".",
-            includes: "_includes", // Sposta qui il file layout.njk se vuoi ordine
-            output: "_site"
-        }
-    };
+  eleventyConfig.addPassthroughCopy("img");
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
 };
